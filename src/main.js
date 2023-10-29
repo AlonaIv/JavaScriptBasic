0386ea26648f38236feb0f67563eda34aa97c9e0
Alona Ivanova <alona20004676@gmail.com>
2023-10-29 21:44:27 +0200

    HW16_Asynchronous_JavaScript

--- src/main.js
+++ src/main.js
@@ -1,51 +1,99 @@
 /**
- * Попрацюємо з числовим паліндромом) 
- * Числовий паліндром - це натуральне число, яке читається зліва направо і справа наліво однаково. 
- * Інакше кажучи, відрізняється симетрією запису (розташування цифр), причому число знаків може бути як парним, так і непарним. 
- * Але Паліндром можна отримати як результат операцій над іншими числами. 
- * Візьмемо будь-яке натуральне число і складемо його зі зворотним числом, тобто записаним тими самими цифрами, але у зворотному порядку. 
- * Проробимо ту саму дію з сумою, що вийшла, і будемо повторювати її доти, доки не утвориться паліндром. 
- * Іноді достатньо зробити всього один крок (наприклад, 312 + 213 = 525), але, як правило, потрібно не менше двох. 
- * Скажімо, число 96 породжує паліндром 4884 тільки на четвертому кроці.... 
- * У результаті я хочу, щоб ви написали функцію, яка повертатиме об'єкт, 
- * де буде властивість result - і це буде паліндром, і властивість steps - це число викликів до знаходження паліндрома. 
- * Для того, щоб перевірити себе використовуйте число 196. Це так зване Lychrel number - число яке немає поліндрому
+ * Створіть функцію randomDelayPrint, яка прийматиме рядок message як аргумент і виводитиме кожну букву цього рядка з довільною затримкою від 0 до 1 секунди. 
+ * Використовуйте setTimeout, щоб додати випадкову затримку перед виведенням кожної літери.
  */
 
-function reverseString(str) {
-  return  str.split('').reverse().join('');
-}
+function randomDelayPrint(message) {
+  const letters = message.split('');
+  let index = 0;
 
-function isPalindrome(number) {
-  const str = number.toString();
-  
-  return str === reverseString(str);
-}
+  function printLetter() {
+    if (index < message.length) {
+      const letter = letters[index];
+      const delay  = Math.random() * 1000;
 
-function isSumPalindrome(number, steps = 1) {
-  if (steps > 6542) {
-    return {
-      result: 'It\'s Lychrel number or too many steps.',
-      steps: steps
+      setTimeout(() => {
+        console.log(letter);
+
+        index++;
+
+        printLetter();
+      }, delay);
     }
   }
 
-  const reverseNumber = parseInt(reverseString(number.toString()));
-  const result        = reverseNumber + number;
-
-  if (true === isPalindrome(result)) {
-    return {
-      result: result,
-      steps: steps
-    }
-  } 
-
-  return isSumPalindrome(result, ++steps);
+  printLetter();
 }
 
-console.log(isSumPalindrome(196));
-console.log(isSumPalindrome(45));
-console.log(isSumPalindrome(312));
-console.log(isSumPalindrome(567));
-console.log(isSumPalindrome(485));
-console.log(isSumPalindrome(7689));
+randomDelayPrint('test str');
+
+/**
+ * Створіть функцію debounce, яка приймає функцію зворотного виклику і затримку (в мілісекундах) як аргументи. 
+ * Функція debounce повинна повертати нову функцію, яка викликає вихідну функцію тільки після того, 
+ * як минула вказана кількість часу без викликів. Це дасть змогу ігнорувати часті виклики функції та виконувати її лише один раз через зазначену затримку після останнього виклику.
+ */
+
+function debounce(callback, delay) {
+  let timeout;
+
+  return function () {
+    const thisArg = this;
+    const args    = arguments;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      callback.apply(thisArg, args);
+    }, delay);
+  };
+}
+
+const expensiveOperation = () => console.log("Виконую складну операцію..."); 
+
+const debouncedExpensiveOperation = debounce(expensiveOperation, 1000);
+
+debouncedExpensiveOperation();
+debouncedExpensiveOperation();
+debouncedExpensiveOperation();
+
+/**
+ * Створіть функцію intervalRace, яка прийматиме масив функцій та інтервал часу t у мілісекундах. 
+ * Функція intervalRace має викликати кожну функцію з масиву по черзі через заданий інтервал часу t. 
+ * Коли всі функції виконано, intervalRace має повернути масив із результатами.
+ */
+
+function intervalRace(functionsArray, t) {
+  let results = [];
+  let index   = 0;
+
+  function executeFunction() {
+    if (index < functionsArray.length) {
+      const curentFunction = functionsArray[index];
+
+      setTimeout(() => {
+        const currentResult = curentFunction();
+        results[index]      = currentResult;
+        
+        index++;
+
+        if (index === functionsArray.length) {
+          console.log(results);
+        } else {
+          executeFunction();
+        }
+      }, t)
+    }
+  }
+
+  executeFunction();
+}
+
+function foo() {
+  return 'foo';
+}
+
+function boo() {
+  return 'boo';
+}
+
+intervalRace([foo, boo], 2000);
